d3ec663d426e18085eb5cd1a40792fdad465a33d
Miguel Izquierdo <miguelizquierdo35@gmail.com>
2025-04-30 16:55:15 -0600

    CountryMapper S 10.121

---
+++ src/app/country/interfaces/country.interface.ts
@@ -0,0 +1,9 @@
+import { Flags } from './rest-countries.interface';
+export interface Country {
+  cca2: string;
+  flag: string;
+  flagSvg: string;
+  name: string;
+  capital: string;
+  population: number;
+}
