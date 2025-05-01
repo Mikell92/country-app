0ab921df4112cd65d7ce0003aff1acfc0e2eb3d6
Miguel Izquierdo <miguelizquierdo35@gmail.com>
2025-04-30 18:02:35 -0600

    manejo de excepciones con Resources 10.124

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable } from 'rxjs';
+import { map, Observable, catchError, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -16,10 +16,14 @@ export class CountryService {
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
-    return this.http
-      .get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-      .pipe(
-        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp))
-      );
+    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
+      map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      catchError((error) => {
+        console.log('Error fetching ', error);
+        return throwError(
+          () => new Error(`No se pudo obtener países con ese query "${query}"`)
+        );
+      })
+    );
   }
 }
