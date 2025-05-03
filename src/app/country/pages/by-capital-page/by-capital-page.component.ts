3301bcda8653665c5f069ec22d1fbb6d5e5c1016
Miguel Izquierdo <miguelizquierdo35@gmail.com>
2025-05-03 14:18:19 -0600

    CountryApp Intermedio/Avanzado, validar parámetros de query 11.142

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -4,7 +4,7 @@ import { CountryListComponent } from '../../components/country-list/country-list
 import { CountryService } from '../../services/country.service';
 import { of } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -15,6 +15,8 @@ export class ByCapitalPageComponent {
   countryService = inject(CountryService);
 
   activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
   queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
 
   query = linkedSignal(() => this.queryParam);
@@ -22,9 +24,13 @@ export class ByCapitalPageComponent {
   countryResoruce = rxResource({
     request: () => ({ query: this.query() }),
     loader: ({ request }) => {
-      console.log({ query: request.query });
-
       if (!this.query()) return of([]);
+
+      this.router.navigate(['/country/by-capital'], {
+        queryParams: {
+          query: request.query,
+        },
+      });
 
       return this.countryService.searchByCapital(request.query);
     },
--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,9 +1,16 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  linkedSignal,
+  resource,
+  signal,
+} from '@angular/core';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { firstValueFrom, of } from 'rxjs';
 import { CountryService } from '../../services/country.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-country-page',
@@ -13,12 +20,23 @@ import { rxResource } from '@angular/core/rxjs-interop';
 export class ByCountryPageComponent {
   countryService = inject(CountryService);
 
-  query = signal('');
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+
+  query = linkedSignal(() => this.queryParam);
 
   countryResoruce = rxResource({
     request: () => ({ query: this.query() }),
     loader: ({ request }) => {
       if (!this.query()) return of([]);
+
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          query: request.query,
+        },
+      });
 
       return this.countryService.searchByCountry(request.query);
     },
